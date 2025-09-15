36324f1541e33c3a0bc8498417b3f0df24aba39f
dwinz14 <136470816+dwinz14@users.noreply.github.com>
2025-09-15 22:52:39 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+  base: "/udin-arum/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
 });
